def6da9b04bd7e2a5427984bc0fbc9964888d447
ReyhanBarindik <reyhanebarindik@icloud.com>
2020-03-19 01:56:30 -0400

    I added codes of IQ05

--- src/com/syntax/InterviewQuestions/InterviewQ05.java
+++ src/com/syntax/InterviewQuestions/InterviewQ05.java
@@ -5,6 +5,19 @@ public class InterviewQ05 {
 	public static void main(String[] args) {
 		// Write a java program to reverse String? Reverse a string word by word?
 
+		String str= "I am a student in Syntax Technologies";
+		String rts="";
+		
+		char [] arr= str.toCharArray();
+		
+		for (int i=str.length()-1; i>=0; i--) {
+			
+			rts= rts + arr[i] ;
+		 //System.out.print(arr[i] +"");
+			
+		}
+		
+		  System.out.println(rts);
 		
 		
 		
