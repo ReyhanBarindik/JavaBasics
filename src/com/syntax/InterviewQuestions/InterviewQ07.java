955404f77b15f9033f958655786bd280e6e5a83a
ReyhanBarindik <reyhanebarindik@icloud.com>
2020-03-19 22:33:59 -0400

    I added IQ07

--- src/com/syntax/InterviewQuestions/InterviewQ07.java
+++ src/com/syntax/InterviewQuestions/InterviewQ07.java
@@ -5,25 +5,28 @@ public class InterviewQ07 {
 	public static void main(String[] args) {
 		// Write a java program to find the second largest number in the array? Maximum and minimum number in the array?
 
-		int [] arr = {100, 20, 70, 30, 40, 10, 50, 6};
+		int [] arr = {80, 20, 100, 30, 40, 10, 50, 6};
 		
 		int maxNum=arr[0];
 		int secondLargestNum=arr[0];
 		int minNum=arr[0];
 		
-		for (int i=0; i<arr.length; i++) {
-			if (arr[i]>maxNum) {
-				secondLargestNum= maxNum;                 // why we equals them ??? When I am delete it, nothing change. 
-				maxNum=arr[i];
-				
-			}else if (arr[i]> secondLargestNum) {
-				secondLargestNum=arr[i];
-				
-			} else if (arr[i]<minNum) {
-				minNum=arr[i];
+	    
+		for (int a : arr) {
+			if (a>maxNum) {
+				maxNum=a;
+			} else if (a<minNum) {
+				minNum=a;
 			}
-			
 		}
+		
+		for (int a : arr) {
+			if (a>secondLargestNum &&  a != maxNum) {
+				secondLargestNum=a;
+			}
+		}
+		
+		
 		System.out.println("Max num of arr array is "+maxNum);
 		System.out.println("Min num of arr array is "+minNum);
 		System.out.println("Second largest num of arr array is "+secondLargestNum);
