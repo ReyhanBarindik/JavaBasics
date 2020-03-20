0dda5d4a8ad6e31c77aa64584ab3201d3cf57a01
ReyhanBarindik <reyhanebarindik@icloud.com>
2020-03-19 20:05:47 -0400

    I changed codes of InterviewQ06

--- src/com/syntax/InterviewQuestions/InterviewQ06.java
+++ src/com/syntax/InterviewQuestions/InterviewQ06.java
@@ -9,28 +9,27 @@ public class InterviewQ06 {
 		
 		Scanner scan;
 		String str;
-		
+		String reverse="";
+		boolean trueF=false;
 		scan=new Scanner (System.in);
 		System.out.println("Please enter your String");
 		str=scan.nextLine();
-		int i=0;
-		int j=str.length()-1;
 		
-		while (i<j) {
-			
-			if (str.charAt(i) != str.charAt(j)) {
-				System.out.println("Your String is Not Palindrome");
-				
-				
-			} else {
-				System.out.println("Your String is Palindrome");
-			}
-			i++;
-			j--;
-			
-			break;
-			
+		String newOne=str.replaceAll("[^A-Za-z]", "");
+		
+		for (int i=newOne.length()-1; i>=0; i--) {
+			reverse=reverse+newOne.charAt(i);
 		}
+		
+		if (newOne.equalsIgnoreCase(reverse)) {
+			trueF=true;
+		} else {
+			trueF=false;
+		}
+		
+		System.out.println(trueF);
+			
+		
 		
 		
 		
